e7b599d4c393bc8a4386828e05231c44ef5ee9a4
nachodll <nachodoradollamas@gmail.com>
2025-06-25 12:48:34 +0200

    reset public schema instead of drop db or tables conditionally

--- pipelines/001_reset_db.py
+++ pipelines/001_reset_db.py
@@ -13,34 +13,29 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    if os.getenv("SKIP_DB_RESET", "").lower() in {"1", "true", "yes"}:
-        logger.info("SKIP_DB_RESET is set - dropping all tables instead of the entire database")
-        subprocess.run(["python", str(DROP_TABLES_PATH)], env=os.environ.copy(), check=True)
-        return
+    logger.info("Resetting the public schema...")
 
-    logger.info("Resetting the database...")
-
-    # The template SQL script is loaded and variables are resolved to environment variables
-    with open(TEMPLATE_PATH) as f:
-        sql_script = os.path.expandvars(f.read())
+    # SQL to drop and recreate the public schema
+    sql_script = "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 
     host = os.getenv("DB_HOST")
     port = os.getenv("DB_PORT")
-    admin_user = os.getenv("DB_ADMIN_USER")
-    admin_password = os.getenv("DB_ADMIN_PASSWORD")
+    user = os.getenv("DB_USER")
+    db_name = os.getenv("DB_NAME")
+    password = os.getenv("DB_PASSWORD")
 
-    # Pasword is set in the environment for psql command
+    # Password is set in the environment for psql command
     env = os.environ.copy()
-    if admin_password:
-        env["PGPASSWORD"] = admin_password
+    if password:
+        env["PGPASSWORD"] = password
 
     # Ensure required environment variables are set
-    if not all([host, port, admin_user]):
-        logger.error("DB_HOST, DB_PORT, and DB_ADMIN_USER environment variables must be set.")
+    if not all([host, port, user]):
+        logger.error("DB_HOST, DB_PORT, and DB_USER environment variables must be set.")
         raise ValueError("Missing required database connection environment variables.")
 
     result = subprocess.run(
-        ["psql", "-v", "ON_ERROR_STOP=1", "-h", str(host), "-p", str(port), "-U", str(admin_user)],
+        ["psql", "-v", "ON_ERROR_STOP=1", "-h", str(host), "-p", str(port), "-U", str(user), "-d", str(db_name)],
         input=sql_script.encode(),
         env=env,
         capture_output=True,
@@ -50,7 +45,7 @@ def main():
         logger.error(f"Failed to reset database: {result.stderr.decode()}")
         raise RuntimeError(f"Database reset failed with error: {result.stderr.decode()}")
 
-    logger.info("Database reset completed")
+    logger.info("Public schema reset completed")
     return
 
 
--- sql/drop_all_tables.sql
+++
@@ -1,8 +0,0 @@
-DO $$
-DECLARE
-    rec record;
-BEGIN
-    FOR rec IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
-        EXECUTE 'DROP TABLE IF EXISTS ' || quote_ident(rec.tablename) || ' CASCADE';
-    END LOOP;
-END $$;
