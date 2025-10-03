dda0e3110bdeba854dbf7dde2914da4beff9dec6
nachodll <nachodoradollamas@gmail.com>
2025-10-03 13:25:26 +0200

    extract data barametros_generales (not validates)

--- pipelines/extract_transform/percepcion_social/001_barometros_generales.py
+++ pipelines/extract_transform/percepcion_social/001_barometros_generales.py
@@ -19,16 +19,49 @@ from utils.normalization import (
 
 RAW_SAV_DIR = Path("data") / "raw" / "CIS" / "CIS004-BarómetrosGenerales"
 CLEAN_CSV_PATH = Path("data") / "clean" / "percepcion_social" / "barometros_generales.csv"
-JSON_VARIABLE_MAP_PATH = Path("data") / "raw" / "CIS" / "CIS004-BarómetrosGenerales" / "CIS_variable_mappings.json"
-LOAD_FROM_RAW = True  # Set to True to load .sav files directly, False to load from pickle for debugging
+JSON_VARIABLE_MAP_PATH = Path("pipelines") / "extract_transform" / "percepcion_social" / "CIS_variable_mappings.json"
+LOAD_FROM_RAW = False  # Set to True to load .sav files directly, False to load from pickle for debugging
 
 
 def get_map_from_json() -> dict[int, dict[str, str]]:
     """Returns a dictionary mapping study codes to variable mappings from the JSON file."""
     with open(JSON_VARIABLE_MAP_PATH, "r", encoding="utf-8") as f:
-        data = json.load(f)
-    print(data)
-    return data
+        variable_mapps = json.load(f)
+    return variable_mapps
+
+
+def get_updated_map() -> dict[int, dict[str, str]]:
+    """Updates the variable mapping with information that could not be automatically extracted."""
+    variable_maps = get_map_from_json()
+
+    for code, mapping in variable_maps.items():
+        # comunidad_autonoma is alwasys mapped to "CCAA" except if its mapped to REGION in the JSON
+        if not ("comunidad_autonoma" in mapping and mapping["comunidad_autonoma"] == "REGION"):
+            mapping["comunidad_autonoma"] = "CCAA"
+
+        # provincia is always mapped to "PROV"
+        mapping["provincia"] = "PROV"
+
+        # breakdown multiple answer questions into separate columns
+        if "problemas_personales" in mapping:
+            original_var = mapping.pop("problemas_personales")
+            for i in range(1, 4):
+                mapping[f"problemas_personales_{i}"] = f"{original_var[:-2]}_{i}"
+
+                # starting from study 3309, problems are mapped to standard names
+                if int(code) >= 3309:
+                    mapping[f"problemas_personales_{i}"] = f"PPERSONAL_{i}"
+
+        if "problemas_generales" in mapping:
+            original_var = mapping.pop("problemas_generales")
+            for i in range(1, 4):
+                mapping[f"problemas_generales_{i}"] = f"{original_var[:-2]}_{i}"
+
+                # starting from study 3309, problems are mapped to standard names
+                if int(code) >= 3309:
+                    mapping[f"problemas_generales_{i}"] = f"PESPANNA_{i}"
+
+    return variable_maps
 
 
 def load_all_sav_files(directory: Path) -> dict[str, pd.DataFrame]:
@@ -37,30 +70,33 @@ def load_all_sav_files(directory: Path) -> dict[str, pd.DataFrame]:
     dataframes_dict = {}
     no_sav_subdirs = []
     corrupted_sav_files = []
-    for subdir in sorted(directory.iterdir()):
-        if subdir.is_dir():
-            sav_files = [f for f in subdir.iterdir() if f.is_file() and f.suffix == ".sav"]
-            if not sav_files:
-                no_sav_subdirs.append(subdir)  #  type: ignore
-            for sav_file in sav_files:
-                try:
-                    df_study = pd.read_spss(sav_file)
-                    study_code = subdir.name[2:6]
-                    if study_code.isdigit() and len(study_code) == 4:
-                        dataframes_dict[study_code] = df_study
-                    else:
-                        logging.warning(f"Skipping file with unexpected name: {sav_file}")
-                except Exception as e:
-                    logging.error(f"Error processing file {sav_file}: {e}")
-                    corrupted_sav_files.append(subdir)  # type: ignore
-                    continue
-
-    print(f"Number of subdirectories: {len(list(directory.iterdir()))}")
-    print(f"Number of files processed: {len(dataframes_dict)}")  # type: ignore
-    print(f"Number of subdirectories without .sav files: {len(no_sav_subdirs)}")  # type: ignore
-    print(f"Number of corrupted .sav files: {len(corrupted_sav_files)}")  # type: ignore
-    print(f"Subdirectories without .sav files: {[subdir.name for subdir in no_sav_subdirs]}")  # type: ignore
-    print(f"Corrupted .sav files: {[subdir.name for subdir in corrupted_sav_files]}")  # type: ignore
+    sorted_subdirs = sorted([d for d in directory.iterdir() if d.is_dir()])
+    for subdir in sorted_subdirs:
+        sav_files = [f for f in subdir.iterdir() if f.is_file() and f.suffix == ".sav"]
+        if not sav_files:
+            no_sav_subdirs.append(subdir)  #  type: ignore
+            continue
+        for sav_file in sav_files:
+            try:
+                df_study = pd.read_spss(sav_file)
+                study_code = subdir.name[2:6]
+                if study_code.isdigit() and len(study_code) == 4:
+                    dataframes_dict[study_code] = df_study
+                else:
+                    logging.warning(f"Skipping file with unexpected name: {sav_file}")
+            except Exception as e:
+                logging.error(f"Error processing file {sav_file}: {e}")
+                corrupted_sav_files.append(subdir)  # type: ignore
+                continue
+    summary = (
+        f"Number of subdirectories: {len(sorted_subdirs)}\n"
+        f"Number of files processed: {len(dataframes_dict)}\n"  # type: ignore
+        f"Number of subdirectories without .sav files: {len(no_sav_subdirs)}\n"  # type: ignore
+        f"Number of corrupted .sav files: {len(corrupted_sav_files)}\n"  # type: ignore
+        f"Subdirectories without .sav files: {[subdir.name for subdir in no_sav_subdirs]}\n"  # type: ignore
+        f"Corrupted .sav files: {[subdir.name for subdir in corrupted_sav_files]}"  # type: ignore
+    )
+    print(summary)
 
     return dataframes_dict  # type: ignore
 
@@ -76,30 +112,47 @@ def main():
             with open("data/debug/barometros_generales_dict.pkl", "rb") as f:
                 df_dict = pickle.load(f)
 
-        # Load variable mapping from JSON. Each study code maps to a dict of {standard_name: original_name}
-        studies_variable_map = get_map_from_json()
+        # Load variable mapping from JSON (with updates). Each study code maps to a dict of {standard_name: original_name}
+        studies_variable_map = get_updated_map()
 
         # For each dataframe, rename columns according to the mapping and concatenate
+        missing = []
         all_dfs = []
         for code, df in df_dict.items():
             mapping = studies_variable_map.get(code)  # type: ignore
             df_study = pd.DataFrame()
             if not mapping:
-                # print("No mapping found for study code:", code)
+                print("No mapping found for study code:", code)
                 continue
 
-            print(f"Processing study code: {code}")
-
             for standard_name, original_name in mapping.items():
-                if original_name in df.columns:
-                    df_study[standard_name] = df[original_name]
+                if standard_name not in ["fecha", "url"]:
+                    if original_name in df.columns:
+                        df_study[standard_name] = df[original_name]
+                    elif standard_name.startswith("problemas_personales") or standard_name.startswith(
+                        "problemas_generales"
+                    ):  # type: ignore
+                        variant_original_name = original_name.replace("_", "0")
+                        if variant_original_name in df.columns:
+                            df_study[standard_name] = df[variant_original_name]
+                    elif original_name.lower() in df.columns:
+                        df_study[standard_name] = df[original_name.lower()]
+                    elif standard_name not in [
+                        "provincia",
+                        "comunidad_autonoma",
+                    ]:
+                        missing.append(code)  # type: ignore
+                        print(
+                            f"Column '{original_name}' ({standard_name}) not found in study {code}: {mapping.get('url')}"
+                        )
 
             df_study["codigo_estudio"] = code
             df_study["fecha"] = mapping["fecha"]
-
             all_dfs.append(df_study)  # type: ignore
 
         df = pd.concat(all_dfs, ignore_index=True)  # type: ignore
+
+        print(f"{len(missing)} missing columns in {len(set(missing))} studies")  # type: ignore
 
         # Save to clean CSV
         CLEAN_CSV_PATH.parent.mkdir(parents=True, exist_ok=True)
